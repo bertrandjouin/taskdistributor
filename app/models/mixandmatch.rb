8434fd4e6c1082d5961b60920a4de0456e3e7ad4
Bertrand Jouin <bertrandjouin@gmail.com>
2020-03-24 14:25:25 +0100

    ok

---
+++ app/models/mixandmatch.rb
@@ -0,0 +1,3 @@
+class Mixandmatch < ApplicationRecord
+  has_many :attibutions
+end
---
+++ db/migrate/20200324110049_create_mixandmatches.rb
@@ -0,0 +1,8 @@
+class CreateMixandmatches < ActiveRecord::Migration[6.0]
+  def change
+    create_table :mixandmatches do |t|
+
+      t.timestamps
+    end
+  end
+end
